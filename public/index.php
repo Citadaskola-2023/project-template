a704009321440620245b288b552470820b767382
Roberts Rozkalns <roberts.rozkalns@me.com>
2024-01-19 11:21:02 +0200

    2024-01-12 datubāzes ievads

--- public/index.php
+++ public/index.php
@@ -2,4 +2,18 @@
 
 require_once  dirname(__DIR__) . '/vendor/autoload.php';
 
+
+
+$db = new \App\Database();
+$posts = $db->query('SELECT * FROM blog_posts WHERE id = 2')
+    ->fetchAll(PDO::FETCH_ASSOC);;
+
+dump($posts);
+
+foreach ($posts as $post) {
+    echo "<li>" . $post['title'] . "</li>";
+}
+
+dd('stop');
+
 view('index');
